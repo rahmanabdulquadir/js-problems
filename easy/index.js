d71b6f2628207530e63e7b788164f8ed5d9ef562
rahmanabdulquadir <rahmanaq777@gmail.com>
2024-10-19 16:51:35 +0600

    problems adding array related

--- easy/index.js
+++ easy/index.js
@@ -1,37 +1,61 @@
 // Convert Celsius to Fahrenheit
 // Write a function that converts Celsius to Fahrenheit.
 const celsiusToFahrenheit = (cel) => {
-  const result = ((cel * 9)/5) + 32
-  return result
-}
+  const result = (cel * 9) / 5 + 32;
+  return result;
+};
 // console.log(celsiusToFahrenheit(3))
 
 // Find the Factorial of a Number
 // Write a function to calculate the factorial of a given number.
-function factorial(num){
+function factorial(num) {
   if (num === 0 || num === 1) return 1;
   return num * factorial(num - 1);
 }
 // console.log(factorial(6))
 
-
 // Sum of Array Elements
 // Write a function that takes an array of numbers and returns the sum of all elements.
 
-const arr = [1,2,3,4,5]
+const arr = [1, 2, 3, 4, 5];
 function sumArray(arr) {
   return arr.reduce((sum, num) => sum + num, 0);
 }
 
 // console.log(sumArray(arr))
 
-
 // extension machine
 
-const arrOfNum = [1,2,3,4,5]
+const arrOfNum = [1, 2, 3, 4, 5];
 
 const exMachine = (arr) => {
-  return arr.map(ob => ob + 5)
+  return arr.map((ob) => ob + 5);
+};
+
+// console.log(exMachine(arrOfNum))
+
+// sum of all elements of an array
+
+const numbers = [10, 20, 30, 40, 50];
+
+function sumArray(array) {
+  const res = numbers.reduce((acc, cur) => {
+    return acc + cur;
+  }, 0);
+  // return res
+  console.log(res)
 }
 
-console.log(exMachine(arrOfNum))
+// sumArray(numbers)
+
+function findMax(arr){
+  for(let i = 0; i > arr.length; i++){
+    console.log(i[i])
+  }
+}
+
+const reverseArray = (arr) => {
+  return arr.reverse()
+}
+
+console.log(reverseArray(numbers))
--- topics/array.js
+++ topics/array.js
@@ -5,25 +5,35 @@
 const myArr = [1, 2, 3, 4, 5]; // array literals
 const myArr2 = new Array(1, 2, 3, 4, 5); //array constructor
 
-myArr[1] //accessing element
-myArr[3] = 7 // replacing an element
+myArr[1]; //accessing element
+myArr[3] = 7; // replacing an element
 // console.log(Array.isArray(myArr))
 
-
-myArr.indexOf(3) // get the index number of any element
-myArr.includes(7) //returns true/false
-myArr.join() // returns string 
-myArr.length // get the length of any array
-myArr[myArr.length - 1] // get the last element of an array
+myArr.indexOf(3); // get the index number of any element
+myArr.includes(7); //returns true/false
+myArr.join(); // returns string
+myArr.length; // get the length of any array
+myArr[myArr.length - 1]; // get the last element of an array
 
 // modify array by different methods
-myArr.push(11) //insert element in the end
-myArr.pop() // remove the last element of an array
-myArr.shift() // removes the first element
-myArr.unshift(1) // adds new element at first
-const sliced = myArr.slice(2, 4) // takes a slice from any array and returns a new sliced array
-// const spliced = myArr.splice(3, 1, 17) // modifies the main array give index number and element number lastly the the new element 
+myArr.push(11); //insert element in the end
+myArr.pop(); // remove the last element of an array
+myArr.shift(); // removes the first element
+myArr.unshift(1); // adds new element at first
+const sliced = myArr.slice(2, 4); // takes a slice from any array and returns a new sliced array
+// const spliced = myArr.splice(3, 1, 17) // modifies the main array give index number and element number lastly the the new element
 
-// map, filter, reduce, forEach
-const result = myArr.map((item) => item * item)
-console.log(result)
+// map, filter, reduce, forEach, sort
+const result = myArr.map((item) => item * item);
+let newArr = [10, 20, 11, 12, 30];
+// const result2 = newArr.filter((item) => {
+//   return (res = item < 20);
+// });
+const randomNum = [10, 20, 30, 40]
+
+const res = randomNum.reduce((acc, cur) => {
+  return sum =  acc + cur
+}, 0)
+
+
+console.log(res);
